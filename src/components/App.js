ec36cd10d83797f522b4dc4d46ff909e0054848e
Rachel Sanham <green_chul@hotmail.com>
2022-02-01 09:19:28 +0000

    initial set up and search component added

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import "../styles/App.css";
+import Search from "./Search";
+
+function App() {
+  return (
+    <div className="App">
+      <img
+        className="nasa-logo"
+        src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
+        alt="nasaLogo"
+      />
+      <Search />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "../styles/Search.css";
+
+const Search = () => {
+  return (
+    <>
+      <input type="text" placeholder="moon" className="search__input" />
+    </>
+  );
+};
+
+export default Search;
